f703f5e6d6e908a2efe38f2ee523dbd44cd69f65
Carlos Benitez <carlos.benitezarciniega@gmail.com>
2026-05-25 10:39:50 -0700

    Creamos una Interfaz en ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Task } from '../models/task.models';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,27 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+
+  // arreglo de tareas
+  tasks: Task[] = [
+    {
+      id: 1,
+      titulo: 'Configuracion ionic',
+      descripcion: 'Instalar Node.js, Angular CLI',
+      finalizado: true,
+      prioridad: 'Alta'
+    },
+    {
+      id: 2,
+      titulo: 'Configurar proyecto',
+      descripcion: 'Crear proyecto con Angular CLI',
+      finalizado: true,
+      prioridad: 'Alta'
+    }
+  ];
+
+  constructor() {
+    console.log(this.tasks);
+  }
+
 }
